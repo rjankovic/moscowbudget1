df67c1bf10c042577f2a8b1e0f48e7003e5efe9f
Radovan Jankovic <jankovic.rj@gmail.com>
2019-11-20 23:27:46 +0100

    top bar

--- src/App.vue
+++ src/App.vue
@@ -6,34 +6,42 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
+        <span>Moscow Budgeting 2020</span>
+        <!-- <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
           transition="scale-transition"
           width="40"
-        />
+        /> -->
 
-        <v-img
+        <!-- <v-img
           alt="Vuetify Name"
           class="shrink mt-1 hidden-sm-and-down"
           contain
           min-width="100"
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
-        />
+        /> -->
       </div>
 
       <v-spacer></v-spacer>
-
+<!-- 
+href="https://github.com/vuetifyjs/vuetify/releases/latest" -->
+      <span class="mr-2">
+        <font-awesome-icon icon="user" />
+      </span>
+      <span class="mr-2">
+        Radovan Jankovic
+      </span>
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span>Logout</span>
+        <!-- <v-icon>mdi-open-in-new</v-icon> -->
         <!-- <font-awesome-icon icon="user-secret" /> -->
       </v-btn>
     </v-app-bar>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,13 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-});
+    theme: {
+      primary: '#4285F4',
+      secondary: '#424242',
+      accent: '#82B1FF',
+      error: '#ff4444',
+      info: '#33b5e5',
+      success: '#00C851',
+      warning: '#ffbb33'
+    }
+  });
